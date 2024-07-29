b3bf20abc1ce9431a6e122c6688c86aec01c1214
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2024-07-28 20:40:56 -0500

    feat: Add YouTube video transcription and chat functionality
    This commit adds the functionality to transcribe YouTube videos and initiate a chat based on the generated transcript. It includes the following changes:
    
    - Added dependencies for AssemblyAI, go-simplejson, backoff, regexp2, goja, sourcemap, go-querystring, pprof, youtube/v2, compress, and golang.org/x/text
    - Implemented the downloadVideo function to download YouTube videos
    - Implemented the extractYouTubeID function to extract the video ID from a YouTube URL
    - Added the generateResponse function to send chat requests to the Ollama Chat API and display the responses
    - Updated the main function to prompt for a YouTube video URL, download the video, transcribe it, and initiate a chat based on the transcript

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/akinfelami/yt-chat
 
 go 1.21.6
+
+require (
+	github.com/AssemblyAI/assemblyai-go-sdk v1.7.0 // indirect
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
+	github.com/kkdai/youtube/v2 v2.10.1 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
+
+	"github.com/AssemblyAI/assemblyai-go-sdk"
+	"github.com/kkdai/youtube/v2"
 )
 
 const OLLAMA_CHAT_URL = "http://localhost:11434/api/chat"
@@ -57,22 +64,170 @@ func readPrompt() string {
 	return strings.TrimSpace(prompt)
 }
 
+func downloadVideo(videoID string, c *youtube.Client) string {
+	log.Println("Downloading video...")
+	video, err := c.GetVideo(videoID)
+	if err != nil {
+		panic(err)
+	}
+	filePath := video.Title + ".mp4"
+	_, err = os.Open(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("File does not exist, downloading...")
+			formats := video.Formats.WithAudioChannels()
+			stream, _, err := c.GetStream(video, &formats[0])
+			if err != nil {
+				panic(err)
+			}
+			defer stream.Close()
+
+			file, err := os.Create(video.Title + ".mp4")
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
+
+			_, err = io.Copy(file, stream)
+			if err != nil {
+				panic(err)
+			}
+			return video.Title + ".mp4"
+		}
+	}
+	log.Println("File already exists, skipping download...")
+	return video.Title + ".mp4"
+}
+
+func extractYouTubeID(url string) (string, error) {
+	pattern := `(?:youtube\.com\/watch\?v=|youtu.be\/)([^&\?]+)`
+	re := regexp.MustCompile(pattern)
+	match := re.FindStringSubmatch(url)
+	if len(match) < 2 {
+		return "", fmt.Errorf("no YouTube video ID found in the URL")
+	}
+	return match[1], nil
+}
+
+func generateResponse(c chatRequest) {
+	dat, err := json.Marshal(c)
+
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+	}
+
+	req, err := http.NewRequest("POST", OLLAMA_CHAT_URL, bytes.NewBuffer(dat))
+	if err != nil {
+		fmt.Println("Error creating HTTP request:", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	reader := bufio.NewReader(resp.Body)
+	var res strings.Builder
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		for {
+			chunk, err := reader.ReadBytes('\n')
+			if err != nil {
+				if err.Error() == "EOF" {
+					fmt.Println()
+					break
+				}
+			}
+			var ollamaResponse ollamaResponse
+			err = json.Unmarshal(chunk, &ollamaResponse)
+			if err != nil {
+				fmt.Println(err)
+			}
+			resp := ollamaResponse.Message.Content
+			res.WriteString(resp)
+			ch <- resp
+		}
+	}()
+
+	for resp := range ch {
+		fmt.Print(resp)
+	}
+}
+
 func main() {
 
 	fmt.Println("++++++++++++++++++++==++++++++++++++++++++++++++++++++++++")
-	fmt.Println("Welcome to Ollama Chat!")
+	log.Println("Welcome to YT Chat!")
+	fmt.Print("Give me a YouTube video URL: ")
+
+	var videoURL string
+	vid := bufio.NewReader(os.Stdin)
+	videoURL, err := vid.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading prompt:", err)
+	}
+	videoURL = strings.TrimSpace(videoURL)
+
+	videoID, err := extractYouTubeID(videoURL)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Extracted Video ID:", videoID)
+	ytClient := youtube.Client{}
+	filePath := downloadVideo(videoID, &ytClient)
+
+	apiKey := os.Getenv("AA_KEY")
+	ctx := context.Background()
+	aaClient := assemblyai.NewClient(apiKey)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Couldn't open video file:", err)
+	}
+	defer f.Close()
+
+	var generatedText string
+	log.Println("Taking a moment to Transcribe...")
+	ttext, err := os.ReadFile(filePath + ".txt")
+	if err != nil {
+		if os.IsNotExist(err) {
+			transcript, err := aaClient.Transcripts.TranscribeFromReader(ctx, f, nil)
+			if err != nil {
+				log.Fatal("Something bad happened:", err)
+			}
+			generatedText = *transcript.Text
+			// Save transcript to file
+			err = os.WriteFile(filePath+".txt", []byte(generatedText), 0644)
+			if err != nil {
+				log.Fatal("Something bad happened:", err)
+			}
+
+		}
+	} else {
+		generatedText = string(ttext)
+	}
 
 	var c chatRequest
 
 	system := message{
-		Role:    System,
-		Content: "You are a helpful assistant. You will help this user answer questions. You are to be very helpful so if the user gives you an unfinished thought you will help them complete it, making sure to articulate yourself and your thought process.",
+		Role: System,
+		Content: fmt.Sprint(`You are a helpful assistant. 
+			You will help this user answer questions that your users have based on the transcript to a youtube video.
+			You will begin a with very short synopsis of the video. It is important that your synopsis is short. However, when a user begins asking questions, feel free to elaborate in order to satisfy the user.
+			Here is the transcript: `, generatedText),
 	}
 
 	c = chatRequest{
 		Model:   "llama3",
 		Message: []message{system},
 	}
+	generateResponse(c)
 
 	for {
 		prompt := readPrompt()
@@ -86,53 +241,7 @@ func main() {
 			Content: prompt,
 		})
 
-		dat, err := json.Marshal(c)
-
-		if err != nil {
-			fmt.Println("Error marshaling JSON:", err)
-		}
-
-		req, err := http.NewRequest("POST", OLLAMA_CHAT_URL, bytes.NewBuffer(dat))
-		if err != nil {
-			fmt.Println("Error creating HTTP request:", err)
-		}
-
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer resp.Body.Close()
-		reader := bufio.NewReader(resp.Body)
-
-		var res strings.Builder
-		ch := make(chan string)
-		go func() {
-			defer close(ch)
-			for {
-				chunk, err := reader.ReadBytes('\n')
-				if err != nil {
-					if err.Error() == "EOF" {
-						fmt.Println()
-						break
-					}
-				}
-				var ollamaResponse ollamaResponse
-				err = json.Unmarshal(chunk, &ollamaResponse)
-				if err != nil {
-					fmt.Println(err)
-				}
-				resp := ollamaResponse.Message.Content
-				res.WriteString(resp)
-				ch <- resp
-			}
-		}()
-
-		for resp := range ch {
-			fmt.Print(resp)
-		}
+		generateResponse(c)
 
 	}
 
